f74f975ecd28321ca41ea79e1e39d00aa443bd87
KatrinAnoshka <nosha309@mail.ru>
2018-11-03 20:17:42 +0300

    ended 1920 section

--- js/common.js
+++ js/common.js
@@ -6,74 +6,24 @@ $(document).ready(function() {
         $('.menu').toggleClass('menu_state_open');
         $('.our-contacts').toggleClass('hidden-div');
     });
+
+    /*******************  Табы переключатели   *******************/
+	$(".tab__item").not(":first").hide();
+	$(".tabs__wrapper .tab").click(function() {
+		$(".tabs__wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".tab__item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+
+	// if ($('.tab__item').css('display', 'block')){
+	// 	$('table.table .table__row--first').css('width', '140px');
+	// }
      
-   	/* Download file */
-
-   	$(".upfile1").click(function () {
-    	$(".file1").trigger('click');
-	});
-	$(".upfile2").click(function () {
-    	$(".file2").trigger('click');
-	});
-	$(".upfile3").click(function () {
-    	$(".file3").trigger('click');
-	});
-	$(".upfile4").click(function () {
-    	$(".file4").trigger('click');
-	});
-	$(".upfile5").click(function () {
-    	$(".file5").trigger('click');
-	});
-	$(".upfile6").click(function () {
-    	$(".file6").trigger('click');
-	});
-	$(".upfile7").click(function () {
-    	$(".file7").trigger('click');
-	});
-	$(".upfile8").click(function () {
-    	$(".file8").trigger('click');
-	});
-	$(".upfile9").click(function () {
-    	$(".file9").trigger('click');
-	});
-
-	/*  Catalog-main */
-
-	$('.submenu li').click(function(){
-		$divId = $(this).attr('target');
-
-		$('.klishe-row').hide(400);
-		$('#'+$divId).show(400);
-
-		$('.submenu li').removeClass('active');
-		$(this).addClass('active');
-
-	});
-
-	/* Smooth scroll*/
-
-	if( window.innerWidth >= 992 ){
-		$('a[href*=#]:not([href=#])').click(function() {
-			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-				var target = $(this.hash);
-				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-				headerHeight = $('.header').height() + 0;  
-			if (target.length) {
-				$('html,body').animate({
-					scrollTop: target.offset().top - headerHeight
-				}, 800, function() {
-				target.focus();
-			});
-			return false;
-			}
-			}
-		});
-	}
+   	
 
 	/* Pop-Up forms */
 
 	$(".order-call").click(function() {
-		$("#form_back h5").html($(this).text());
+		$("#form_back h4").html($(this).text());
 		$("#form_back input[name=formname]").val($(this).text());
 	}).magnificPopup({
 		type:"inline",
@@ -92,21 +42,7 @@ $(document).ready(function() {
                 $('body').removeClass('loading');
             }
         });
-  //       $('.products .flexslider').flexslider({
-		//     animation: "slide",
-		//     touch: true,		
-	 //  	});
-	 //  	$('.review .flexslider').flexslider({
-		//     animation: "slide",		    
-		//     touch: true
-	 //  	});
-	 //  	$('.flexslider-clients').flexslider({
-		//     animation: "slide",
-		//     touch: true,
-		//     animationLoop: false,
-		//     itemWidth: 210,
-		//     itemMargin: 150
-		// });
+ 
     });
 
 
