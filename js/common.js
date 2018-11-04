c4f8f893f3b9e6eb2f9d2b5fdee6ef87350888d1
KatrinAnoshka <nosha309@mail.ru>
2018-11-04 15:31:05 +0300

    cleaned code

--- js/common.js
+++ js/common.js
@@ -2,29 +2,19 @@ $(document).ready(function() {
 
 	/* Adaptive menu */
    
-    // $('.menu__icon').on('click', function(){
-    //     $('.menu').toggleClass('menu_state_open');
-    //     $('.our-contacts').toggleClass('hidden-div');
-    // });
     $(".menu__icon").click(function() {
 		$(this).toggleClass("on");
 		$(".menu").slideToggle();
-		// $('.our-contacts').toggleClass('hidden-div')
 		return false;
 	});
 
-    /*******************  Табы переключатели   *******************/
+    /*  Tabs  */
+
 	$(".tab__item").not(":first").hide();
 	$(".tabs__wrapper .tab").click(function() {
 		$(".tabs__wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
 		$(".tab__item").hide().eq($(this).index()).fadeIn()
 	}).eq(0).addClass("active");
-
-	// if ($('.tab__item').css('display', 'block')){
-	// 	$('table.table .table__row--first').css('width', '140px');
-	// }
-     
-   	
 
 	/* Pop-Up forms */
 
